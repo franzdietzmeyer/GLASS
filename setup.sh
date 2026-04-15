2583245cd99fb7594dcaf8b4be54aff0a9eec0d6
Bloeci <maxb@LAPTOP-5A1JT0L9.localdomain>
2026-04-15 12:03:10 +0200

    Add mac/linux parts

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ fi
 JAVA_HOME="$("$VENV_DIR/bin/python" -c "
 import jdk, os, glob
 d = sorted(glob.glob(os.path.join('$JDK_BASE', 'jdk-*')))[0]
-system=jdk.OS
+system=str(jdk.OS)
 print(os.path.join(d, 'Contents', 'Home') if 'mac' in system else d)
 ")"
 
